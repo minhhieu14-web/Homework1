b199982b64fe2cdff335dabb5abbbb0208aec946
JosephLe <minhhieu14012006@gmail.com>
2025-08-14 12:08:12 +0700

    Add files via upload

---
+++ Exercise22.java
@@ -0,0 +1,7 @@
+public class Exercise22 {
+    public static void main(String[] args) {
+        int x=1, z=x - x-- - x++ + --x - x + x-- +x; 
+        System.out.println("x="+x);
+        System.out.println("z="+z);
+    }
+}
---
+++ bai1.java
@@ -0,0 +1,11 @@
+
+public class bai1 {
+    public static void main(String[] args) {
+        System.out.println("1)1+2="+(1+2));
+        System.out.println("2)3.5+8="+(3.5+8));
+        System.out.println("3)1/2="+(1/2));
+        System.out.println("4)4.5+7="+(4.5+7));
+        System.out.println("5)4*5+1="+(4*5+1));
+        System.out.println("6)20%3+2-7="+(20%3+2-7));
+    }
+}
